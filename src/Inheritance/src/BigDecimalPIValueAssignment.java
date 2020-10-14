f56e446394f3749bbe267909e9e23b15766a2f79
nil <nil@192.168.0.14>
2020-10-14 14:51:28 -0700

    Inheritance Example on Time and Date plus BigInteger

---
+++ src/Inheritance/src/BigDecimalPIValueAssignment.java
@@ -0,0 +1,18 @@
+package Inheritance.src;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class BigDecimalPIValueAssignment {
+	
+	
+	public static void main(String args[]) {
+   
+    BigDecimal pi = new BigDecimal(Math.PI);
+     
+    System.out.println("Pi Value = "+pi);
+    pi = pi.setScale(1000,RoundingMode.CEILING);
+    System.out.println("Pi Value upto 1000 = "+pi);
+   
+	}
+}
